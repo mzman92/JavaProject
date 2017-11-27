9c54f8809c44f475a71f4696f7a3ebea2e05fed6
mzman92 <aby.ndhlovu@gmail.com>
2017-11-27 12:05:42 +0200

    Added my java project to Ripo

---
+++ CountFrom1To10.java
@@ -0,0 +1,13 @@
+
+public class CountFrom1To10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1; i<10; i++){
+			System.out.println("i =" + i);
+		}
+			
+			
+	}
+
+}
